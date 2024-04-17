5618dc6087c78e9d1a56134ac9e524356d5122e8
kesavamoorthi-gk <kesavamoorthi@sparkouttech.com>
2024-04-17 18:51:55 +0530

    Table works - logic changes - added filter option - fixed possible errors on combination of search and pagination, filter and pagination - displaying correct info about table - tailwind config changes - added first and last buttons - added S No to the table records

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,7 +1,8 @@
-// table.component.ts
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 interface TableColumn {
   key: string;
@@ -17,7 +18,7 @@ interface TableData {
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
@@ -33,20 +34,51 @@ export class TableComponent implements OnChanges {
   sortDirection: 'asc' | 'desc' = 'asc';
   filterText: string = '';
 
-  private searchTextSubject = new Subject<string>();
+  private searchTextSubject = new BehaviorSubject<string>('');
+  hasNextPage: boolean = false;
+  hasPreviousPage: boolean = false;
+  selectedBrand: string = '';
+  brandFilter = new FormControl('');
 
   constructor() {
-    this.searchTextSubject.pipe(debounceTime(300)).subscribe((text) => {
+    this.searchTextSubject.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe((text) => {
       this.filterText = text;
       this.currentPage = 1;
+      this.updatePaginationState();
       this.pageChange.emit(this.currentPage);
+    });
+    // Subscribe to changes in the brand filter dropdown
+    this.brandFilter.valueChanges.pipe(
+      distinctUntilChanged() // Only emit distinct values
+    ).subscribe((brand: string) => {
+      this.selectedBrand = brand; // Update selectedBrand when dropdown value changes
+      this.currentPage = 1; // Reset to first page when filter changes
+      this.updatePaginationState(); // Update pagination state
     });
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['data']) {
       this.currentPage = this.initialPage;
+      this.updatePaginationState();
     }
+  }
+  onBrandFilterChange() {
+    const selectedBrand = this.brandFilter.value;
+    // Filter data based on the selected brand
+    this.searchTextSubject.next(selectedBrand);
+  }
+
+  getCategoryBrands(): string[] {
+    // return ['Apple', 'Samsung']
+    const brandsSet = new Set<string>();
+    this.data.forEach(item => {
+      brandsSet.add(item['brand']);
+    });
+    return Array.from(brandsSet);
   }
 
   onSearch(event: Event) {
@@ -66,29 +98,72 @@ export class TableComponent implements OnChanges {
 
   onPageChange(page: number) {
     this.currentPage = page;
+    this.updatePaginationState();
     this.pageChange.emit(this.currentPage);
+
   }
 
   getFilteredData(): TableData[] {
     let filteredData = this.data.filter((item) =>
       Object.values(item).some((value) =>
-        value.toString().toLowerCase().includes(this.filterText.toLowerCase())
+        value?.toString().toLowerCase().includes(this.filterText.toLowerCase())
       )
     );
+    // Apply brand filter if selectedBrand is not empty
+    if (this.selectedBrand) {
+      filteredData = filteredData.filter(item => item['brand'] === this.selectedBrand);
+    }
+
 
     if (this.sortColumn) {
       filteredData = filteredData.sort((a, b) => {
-        const x = a[this.sortColumn];
-        const y = b[this.sortColumn];
-        return this.sortDirection === 'asc' ? (x < y ? -1 : x > y ? 1 : 0) : (x < y ? 1 : x > y ? -1 : 0);
+        const x = a[this.sortColumn!];
+        const y = b[this.sortColumn!];
+        return this.sortDirection === 'asc' ? (x === y ? 0 : x < y ? -1 : 1) : (x === y ? 0 : x < y ? 1 : -1);
       });
     }
+
 
     return filteredData;
   }
 
   getPageNumbers(): number[] {
-    const totalPages = Math.ceil(this.getFilteredData().length / this.pageSize);
-    return Array.from({ length: totalPages }, (_, i) => i + 1);
+    const filteredData = this.getFilteredData();
+
+    const totalPages = Math.ceil(filteredData.length / this.pageSize);
+
+    const maxPages = Math.min(totalPages, 10); // Max pages to display initially
+
+    let startPage: number;
+    let endPage: number;
+
+    if (totalPages <= maxPages) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      let maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
+      let maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
+      if (this.currentPage <= maxPagesBeforeCurrentPage) {
+        startPage = 1;
+        endPage = maxPages;
+      } else if (this.currentPage + maxPagesAfterCurrentPage >= totalPages) {
+        startPage = totalPages - maxPages + 1;
+        endPage = totalPages;
+      } else {
+        startPage = this.currentPage - maxPagesBeforeCurrentPage;
+        endPage = this.currentPage + maxPagesAfterCurrentPage;
+      }
+    }
+
+    return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
+    // return Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+  }
+
+  updatePaginationState() {
+    const filteredData = this.getFilteredData();
+    const totalPages = Math.ceil(filteredData.length / this.pageSize);
+
+    this.hasNextPage = this.currentPage < totalPages;
+    this.hasPreviousPage = this.currentPage > 1;
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,13 @@ module.exports = {
         primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" },
       }
     },
+    backgroundColor:
+    {
+      primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#000000", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" },
+    },
     fontFamily: {
       'body': [
-    'Inter', 
+    'Source Sans Pro', 
     'ui-sans-serif', 
     'system-ui', 
     '-apple-system', 
@@ -29,7 +33,7 @@ module.exports = {
     'Noto Color Emoji'
   ],
       'sans': [
-    'Inter', 
+    'Source Sans Pro', 
     'ui-sans-serif', 
     'system-ui', 
     '-apple-system', 
